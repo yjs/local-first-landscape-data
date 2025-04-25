72b4eaa8def8a8a056f6c41a9c2a9eb3d057d281
Kevin Jahns <kevin.jahns@protonmail.com>
2025-04-25 15:55:11 +0200

    fill out form

--- data.js
+++ data.js
@@ -3,39 +3,76 @@ import { LandscapeSchema } from '@localfirstfm/landscape-schema'
 
 export const data = LandscapeSchema.make({
 	Version: 1,
-	Id: 'supersync',
-	Name: 'SuperSync',
-	Description: 'A cool way to sync data',
-	Website: 'https://supersync.cool',
-	Deployment: ['Self-hosted'],
+	Id: 'yjs',
+	Name: 'Yjs',
+	Description: 'Shared types to make anything collaborative',
+	Website: 'https://yjs.dev',
+  GitHub: 'https://github.com/yjs/yjs',
+  GetStarted: 'https://learn.yjs.dev',
+	Deployment: ['Self-hosted', 'Hosted', 'Third-party integrations'],
 	License: 'MIT',
 	AppTarget: {
-		LanguageSDK: { data: ['typescript'] },
+		LanguageSDK: { data: ['TypeScript', 'JavaScript', 'Rust', 'C#', 'Swift', 'Python', 'Cffi', 'Kotlin'] },
+    Platform: { data: ['Browser', 'Node', 'iOS', 'React Native', 'WASM'], comment: 'We have language bindings to many popular programming languages. See y-crdt.' },
+    FrameworkIntegrations: {
+      data: ['React', 'React Native', 'Vue', 'Svelte', 'Zustand'], 
+      comment: 'There are several "bindings" that make Yjs work with different web frameworks. This list is probably not up to date.'
+    },
+    ClientBundleSize: {
+      data: '~20kb', comment: 'About ~20kb gzipped. Your final bundle might be smaller as Yjs works well with dead-code elimination.'
+    }
 	},
 	Networking: {
-		Topology: { data: 'Client-Server' },
+		Topology: { data: 'P2P / Client-server' },
+    Protocol: {
+      data: ['WebRTC', 'WebSockets', 'Matrix', 'Signal', 'via integrations', 'custom'],
+      comment: 'It is relatively easy sync Yjs through custom communication protocols.' 
+    }
 	},
 	ServerSideData: {
-		PersistenceMechanism: { data: ['N/A'] },
-		DataModelParadigm: { data: 'Relational' },
+    PersistenceMechanism: {
+      data: ['Redis', 'Leveldb', 'DB-integrations', 'custom'], 
+      comment: 'It is relatively easy to persist Yjs data in your own database.' 
+    },
+		DataModelParadigm: { data: 'Document' },
+    ExistingDatabaseSupport: { data: 'Via integrations and plugins' }
 	},
+  MaturityLevel: 'Mature',
+  InitialReleaseDate: new Date('Tue Jan 27 2015 00:00:00 GMT+0100 (Central European Standard Time)'),
+  NotableAdopters: [{ Name: 'Evernote', URL: 'https://evernote.com' }, { Name: 'Project Jupyter', URL: 'https://jupyter.org/' }, { Name: 'La Suite', URL: 'https://lasuite.numerique.gouv.fr/en' }, { Name: 'NextCloud', URL: 'https://nextcloud.com/' }],
+  UniquenessNote: 'Minimal, flexible, fast, easy to integrate, unopinionated',
 	ClientSideData: {
-		QueryAPI: { data: ['Async'] },
-		PersistenceMechanism: { data: ['IndexedDB', 'OPFS'] },
-		PersistenceFeatures: { data: 'Indexes' },
+		QueryAPI: { data: ['Sync'] },
+		PersistenceMechanism: { data: ['IndexedDB', 'DB-integrations', 'custom'] },
+		PersistenceFeatures: { data: 'Transactions' },
 		DataModel: { data: 'Document' },
 		OfflineReads: { data: 'Full Support' },
 		OfflineWrites: { data: 'Local conflict resolution' },
-		DataSize: { data: 'Up to 5-10 MB per document' },
+		DataSize: { data: 'Depends on the size of the shared data' },
+    OptimisticUpdates: { data: 'Yes' }
 	},
 	SynchronizationStrategy: {
 		FullOrPartialReplication: { data: ['Full Replication'] },
 		ConflictHandling: { data: 'Automatic via CRDT' },
 		WhereResolutionOccurs: { data: 'Client' },
+    Latency: { data: '<1ms', comment: 'Depends on which provider you choose. But it\'s pretty fast.' },
 		WhatGetsSynced: {
 			data: {
-				ClientToClient: 'Ops',
+				ClientToClient: 'CRDT operations, awareness state, content attribution.'
 			},
 		},
+    Authority: { data: 'Decentralized' }
 	},
+  AuthIdentity: {
+    Encryption: { data: 'Unopinionated, via providers', comment: 'E.g. via secsync or matrix-crdt' },
+    AuthenticationMethod: { data: ['Unopinionated, via providers'] },
+    AuthorizationPermissions: { data: 'Unopinionated, via providers' }
+  },
+  UIRelated: {
+    RichTextEditing: { data: 'Yes', comment: 'We have integrations with most popular rich-text editors like ProseMirror, Lexical, and CodeMirror.' }
+  },
+  DevelopmentWorkflowsDX: {
+    DebuggingTools: { data: ['DevTools', 'Data Inspector'] },
+    TypeSupport: { data: 'Full Support' }
+  }
 })
--- details.md
+++ details.md
@@ -1,15 +1,7 @@
-# SuperSync
+# Yjs
 
-SuperSync is a cool way to sync your data.
+Sponsors-backed project for making anything on the web collaborative. Connect
+with us to build cool integrations.
 
-## Features
+Visit [yjs.dev](https://yjs.dev) for more information.
 
-- **SuperSync**: Sync your data with ease.
-- **Local-first**: Your data is stored locally, ensuring privacy and security.
-- **Cross-platform**: Works on all major platforms.
-- **Open-source**: The code is available for anyone to use and contribute to.
-- **Community-driven**: Join our community and help us improve the project.
-
-## More info
-
-...
